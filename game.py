6b577d5328b60e1daf6cb06f6f23d1dcf40a9d34
selrahcsan <charlesstna@proton.me>
2025-10-03 19:06:21 -0300

    Criado a janela incial do jogo

---
+++ game.py
@@ -0,0 +1,13 @@
+WHIDTH = 1280
+HEIGHT = 768
+
+def draw():
+    """Desenha a Tela"""
+    screen.fill((128, 0, 0))
+    
+def main():
+    """Inicia o Jogo"""
+    pgzero.go()
+
+if __name__ == '__main__':
+    main()
