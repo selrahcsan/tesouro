685c930271f929873e262780f07812963927ad62
selrahcsan <charlesstna@proton.me>
2025-10-05 15:59:02 -0300

    Inserido musica, ao executar a janela

--- game.py
+++ game.py
@@ -7,6 +7,13 @@ TITLE = 'Caça ao Tesouro'
 game_state = "menu"
 music_on = True
 
+try:
+    music.play("background_music.mp3")
+    music.set_volume(0.3)
+except Exception:
+    print("Arquivo 'background_music.mp3' não encontrado na pasta music/. A música não será tocada.")
+
+
 def draw():
     """Desenha a Tela"""
     screen.fill((46, 204, 113))
